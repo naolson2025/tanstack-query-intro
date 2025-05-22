ac884505b72510fc614540ddbdd64ebf07022d36
Nick Olson <45021396+naolson2025@users.noreply.github.com>
2025-05-22 08:11:38 -0500

    finished demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+// import { useEffect, useState } from 'react';
 
 type Book = {
   id: number;
@@ -7,40 +8,47 @@ type Book = {
 };
 
 function App() {
-  const [books, setBooks] = useState<Book[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-  const [counter, setCounter] = useState(0);
+  const { data, isError, isLoading, error } = useQuery<Book[]>({
+    queryKey: ['books'],
+    queryFn: async () => {
+      const resp = await fetch('/api/books');
+      if (!resp.ok) throw new Error('Failed to fetch books');
+      return resp.json();
+    },
+  });
+  // const [books, setBooks] = useState<Book[]>([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [isError, setIsError] = useState(false);
+  // const [error, setError] = useState<Error | null>(null);
 
-  useEffect(() => {
-    setIsLoading(true);
-    setIsError(false);
-    setError(null);
-    const fetchBooks = async () => {
-      let attempts = 0;
-      let success = false;
-      while (attempts < 3 && !success) {
-        try {
-          const resp = await fetch('/api/books');
-          if (!resp.ok) {
-            throw new Error('Failed to fetch books');
-          }
-          const data = await resp.json();
-          setBooks(data);
-          success = true;
-        } catch (err) {
-          attempts += 1;
-          if (attempts === 3) {
-            setIsError(true);
-            setError(err instanceof Error ? err : new Error('Unknown error'));
-          }
-        }
-      }
-      setIsLoading(false);
-    };
-    fetchBooks();
-  }, []);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   setIsError(false);
+  //   setError(null);
+  //   const fetchBooks = async () => {
+  //     let attempts = 0;
+  //     let success = false;
+  //     while (attempts < 3 && !success) {
+  //       try {
+  //         const resp = await fetch('/api/books');
+  //         if (!resp.ok) {
+  //           throw new Error('Failed to fetch books');
+  //         }
+  //         const data = await resp.json();
+  //         setBooks(data);
+  //         success = true;
+  //       } catch (err) {
+  //         attempts += 1;
+  //         if (attempts === 3) {
+  //           setIsError(true);
+  //           setError(err instanceof Error ? err : new Error('Unknown error'));
+  //         }
+  //       }
+  //     }
+  //     setIsLoading(false);
+  //   };
+  //   fetchBooks();
+  // }, []);
 
   if (isLoading) {
     return (
@@ -63,20 +71,12 @@ function App() {
     <div className="p-20">
       <h1 className="flex justify-center text-4xl m-4">Book List</h1>
       <ul className="list bg-base-200 rounded-box shadow-md">
-        {books.map((book: Book) => (
+        {data?.map((book: Book) => (
           <li key={book.id} className="list-row">
             {book.title} by {book.author}
           </li>
         ))}
       </ul>
-      <div className="flex justify-end">
-        <button
-          className="btn btn-primary mt-4"
-          onClick={() => setCounter((prev) => prev + 1)}
-        >
-          Click me! {counter}
-        </button>
-      </div>
     </div>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-// import react-query
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-// add query client
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
